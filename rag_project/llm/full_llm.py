f2f0829aec2e0479c9efb481b57e40534fb6d59c
pranavtamada <pranav.tamada@gmail.com>
2026-04-16 15:33:01 +0530

    Update full_llm.py

--- rag_project/llm/full_llm.py
+++ rag_project/llm/full_llm.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 import time
 
-genai.configure(api_key="AQ.Ab8RN6IY2D0zC-FjSEBwRRIPd_bktoW1yt7ZY0UEakrGb4TtJA")
+genai.configure(api_key="API_KEY")
 
 model = genai.GenerativeModel("gemini-2.5-flash-lite")
 
